7d72f3418e29b587ce4ccc1e2de69d8c406fbd0e
Cloud <Cloud@clouddemacbook-pro.local>
2016-09-08 20:37:22 +0800

    Init

---
+++ BarrageDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BarrageDemo
+//
+//  Created by Cloud on 16/9/8.
+//  Copyright © 2016年 Cloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BarrageDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BarrageDemo
+//
+//  Created by Cloud on 16/9/8.
+//  Copyright © 2016年 Cloud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
